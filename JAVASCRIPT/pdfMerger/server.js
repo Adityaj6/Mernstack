535dc9b2cca94397cd8d674b94e27776aef5b659
Aditya Srivastava <adityaji682@gmail.com>
2023-09-10 15:53:22 +0530

    kjsut some comment

--- JAVASCRIPT/pdfMerger/server.js
+++ JAVASCRIPT/pdfMerger/server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname,"templates/index.html"))
 })
+
+app.post('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"templates/index.html"))
+})
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+    // req.files is array of `photos` files
+    // req.body will contain the text fields, if there were any
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
